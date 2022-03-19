e8c53f0b63b2b54bc5f78aa54ac0d88cb60b2b29
LizaKhvesko <khveskoliza@tut.by>
2022-03-19 14:16:12 +0300

    add validation, submit forms, calc balcons

--- dist/js/main.js
+++ dist/js/main.js
@@ -16,7 +16,17 @@
   \******************/
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
-eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_modal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modal */ \"./modules/modal.js\");\n/* harmony import */ var _modules_timer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/timer */ \"./modules/timer.js\");\n/* harmony import */ var _modules_scroll__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/scroll */ \"./modules/scroll.js\");\n\n\n\n\n(0,_modules_modal__WEBPACK_IMPORTED_MODULE_0__.modal)();\n(0,_modules_timer__WEBPACK_IMPORTED_MODULE_1__.timer)('27 march 2022');\n(0,_modules_scroll__WEBPACK_IMPORTED_MODULE_2__.scroll)();\n\n\n//# sourceURL=webpack:///./index.js?");
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony import */ var _modules_modal__WEBPACK_IMPORTED_MODULE_0__ = __webpack_require__(/*! ./modules/modal */ \"./modules/modal.js\");\n/* harmony import */ var _modules_timer__WEBPACK_IMPORTED_MODULE_1__ = __webpack_require__(/*! ./modules/timer */ \"./modules/timer.js\");\n/* harmony import */ var _modules_scroll__WEBPACK_IMPORTED_MODULE_2__ = __webpack_require__(/*! ./modules/scroll */ \"./modules/scroll.js\");\n/* harmony import */ var _modules_validation__WEBPACK_IMPORTED_MODULE_3__ = __webpack_require__(/*! ./modules/validation */ \"./modules/validation.js\");\n/* harmony import */ var _modules_sendForm__WEBPACK_IMPORTED_MODULE_4__ = __webpack_require__(/*! ./modules/sendForm */ \"./modules/sendForm.js\");\n/* harmony import */ var _modules_calc__WEBPACK_IMPORTED_MODULE_5__ = __webpack_require__(/*! ./modules/calc */ \"./modules/calc.js\");\n\n\n\n\n\n\n\n(0,_modules_modal__WEBPACK_IMPORTED_MODULE_0__.modal)();\n(0,_modules_timer__WEBPACK_IMPORTED_MODULE_1__.timer)('27 march 2022');\n(0,_modules_scroll__WEBPACK_IMPORTED_MODULE_2__.scroll)();\n(0,_modules_validation__WEBPACK_IMPORTED_MODULE_3__.validation)();\n(0,_modules_calc__WEBPACK_IMPORTED_MODULE_5__.calc)();\n(0,_modules_sendForm__WEBPACK_IMPORTED_MODULE_4__.sendForm)({\n    classForm: '.form-horizontal',\n    addInfo: [{\n        type: 'input',\n        id: 'calc-total'\n    }]\n});\n\n\n\n//# sourceURL=webpack:///./index.js?");
+
+/***/ }),
+
+/***/ "./modules/calc.js":
+/*!*************************!*\
+  !*** ./modules/calc.js ***!
+  \*************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"calc\": () => (/* binding */ calc)\n/* harmony export */ });\nconst calc = () => {\n    const calcBlock = document.getElementById('calc');\n    const type = document.getElementById('calc-type');\n    const material = document.getElementById('calc-type-material');\n    const square = document.getElementById('calc-input');\n    const total = document.getElementById('calc-total');\n\n    function countTotal() {\n        if (type.options[0].selected !== true && material.options[0].selected !== true && square.value !== '') {\n            total.value = +square.value * type.options[type.selectedIndex].value * material.options[type.selectedIndex].value\n        } else {\n            total.value = '0';\n        }\n    }\n\n    calcBlock.addEventListener('change', (e) => {\n        if (e.target === type || e.target === material || e.target === square) {\n            countTotal();\n        }\n    })\n}\n\n//# sourceURL=webpack:///./modules/calc.js?");
 
 /***/ }),
 
@@ -40,6 +50,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 
 /***/ }),
 
+/***/ "./modules/sendForm.js":
+/*!*****************************!*\
+  !*** ./modules/sendForm.js ***!
+  \*****************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"sendForm\": () => (/* binding */ sendForm)\n/* harmony export */ });\nconst sendForm = ({classForm, addInfo = []}) => {\n    const forms = document.querySelectorAll(classForm);\n    forms.forEach(form => {\n        const inputs = Array.from(form.querySelectorAll('input'));\n        inputs.forEach(input => input.required = true)\n\n        const sendData = (data) => {\n            return fetch('https://jsonplaceholder.typicode.com/posts', {\n                method: 'POST',\n                body: JSON.stringify(data),\n                headers: {\n                    \"Content-Type\": \"application/json\" \n                }}).then(res => res.json())\n        }\n\n        const submitForm = () => {\n            if (inputs.every(input => input.style.border !== '3px solid red')) {\n                const formData = new FormData(form);\n                const formBody = {};\n\n                formData.forEach((val, key) => {\n                    formBody[key] = val; \n                })\n\n                addInfo.forEach(elem => {\n                const element = document.getElementById(elem.id);\n                if (element) {\n                    if (elem.type === 'block') {\n                    formBody[elem.id] = element.textContent;\n                    } else if (elem.type === 'input'){\n                    formBody[elem.id] = element.value;  \n                    }\n                }\n            })\n            \n                sendData(formBody)\n                    .then(() => {\n                        inputs.forEach(input => {\n                            input.value = ''\n                        })\n                    })\n                    .catch(error => console.log(error))\n            } else {\n                return\n            }\n        }\n\n        try {\n            if (!form) {\n                throw new Error('Не найдена форма')\n            }\n\n            form.addEventListener('submit', (e) => {\n                e.preventDefault();\n                \n                submitForm()\n            })\n        } catch (error) {\n            console.log(error.message)\n        }\n    })\n}\n\n//# sourceURL=webpack:///./modules/sendForm.js?");
+
+/***/ }),
+
 /***/ "./modules/timer.js":
 /*!**************************!*\
   !*** ./modules/timer.js ***!
@@ -47,6 +67,16 @@ eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpac
 /***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
 
 eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"timer\": () => (/* binding */ timer)\n/* harmony export */ });\nconst timer = (date) => {\n    const daysSpan = document.querySelectorAll('.count_1 span');\n    const hoursSpan = document.querySelectorAll('.count_2 span');\n    const minutesSpan = document.querySelectorAll('.count_3 span');\n    const secondsSpan = document.querySelectorAll('.count_4 span');\n\n    const getRemaining = () => {\n        let dateStop = new Date(date).getTime();\n        let dateNow = new Date().getTime();\n        let timeRemaining = (dateStop - dateNow) / 1000;\n        let days = Math.floor(timeRemaining / 60 / 60 / 24);\n        let hours = Math.floor((timeRemaining / 60 / 60) %24);\n        let minutes = Math.floor((timeRemaining / 60) % 60);\n        let seconds = Math.floor(timeRemaining % 60);\n\n         if (timeRemaining < 0) {\n            timeRemaining = 0;\n            days = 0;\n            hours = 0;\n            minutes = 0;\n            seconds = 0;\n        }\n        \n        return {timeRemaining, days, hours, minutes, seconds}\n    }\n\n    const addZero = (time, spans) => {\n         spans.forEach(span => {\n            time < 10 ? span.textContent = '0' + time : span.textContent = time;\n        })\n    }\n\n    const updateClock = () => {\n        let getTime = getRemaining();\n        \n        addZero(getTime.days, daysSpan);\n        addZero(getTime.hours, hoursSpan);\n        addZero(getTime.minutes, minutesSpan);\n        addZero(getTime.seconds, secondsSpan);\n    }\n\n    const interval = () => {\n        let intervalId;\n        let getTime = getRemaining();\n        updateClock();\n        if(getTime.timeRemaining > 0) {\n            intervalId = setInterval(updateClock, 1000);\n        } else {\n            clearInterval(intervalId)\n        }\n    }\n    interval();\n}\n\n//# sourceURL=webpack:///./modules/timer.js?");
+
+/***/ }),
+
+/***/ "./modules/validation.js":
+/*!*******************************!*\
+  !*** ./modules/validation.js ***!
+  \*******************************/
+/***/ ((__unused_webpack_module, __webpack_exports__, __webpack_require__) => {
+
+eval("__webpack_require__.r(__webpack_exports__);\n/* harmony export */ __webpack_require__.d(__webpack_exports__, {\n/* harmony export */   \"validation\": () => (/* binding */ validation)\n/* harmony export */ });\nconst validation = () => {\n    const names = document.querySelectorAll('input[name=\"fio\"]');\n    const tels = document.querySelectorAll('input[name=\"phone\"]');\n    const calcInput = document.getElementById('calc-input');\n    \n    names.forEach(name => {\n        name.addEventListener('input', (e) => {\n            let reg = /[^a-zа-я]/gi;\n            e.target.value =  e.target.value.replace(reg, '');\n        })\n    })\n\n    tels.forEach(tel => {\n        tel.addEventListener('input', (e) => {\n            let reg = /[^\\d\\+]/g;\n            e.target.value =  e.target.value.replace(reg, '');\n            if (e.target.value.length > 17) {\n               tel.style.border = '3px solid red';\n            } else {\n                tel.style.border = ''\n            }\n        })\n    })\n\n    calcInput.addEventListener('input', (e) => {\n            let reg = /[^\\d]/g;\n            e.target.value =  e.target.value.replace(reg, '');\n        })\n}\n\n//# sourceURL=webpack:///./modules/validation.js?");
 
 /***/ })
 
