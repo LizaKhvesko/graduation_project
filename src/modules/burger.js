11536b8bad811e1bfcf69effef823ed16ae7bf51
LizaKhvesko <khveskoliza@tut.by>
2022-03-21 17:59:45 +0300

    add burger menu

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -1,9 +1,12 @@
 export const burger = () => {
-    const burgerBtn = document.querySelector('.collapsed');
+    const burgerBtn = document.getElementById('navigation');
+    const mobileMenu = burgerBtn.querySelector('.navbar-collapse.collapse');
 
     burgerBtn.addEventListener('click', (e) => {
         if (e.target.closest('.collapsed')) {
-            
+           mobileMenu.classList.toggle('burger-open');
+        } else if (mobileMenu.classList.contains('burger-open') && !e.target.classList.contains('navbar-collapse')) {
+            mobileMenu.classList.remove('burger-open');
         }
     })
 }
--- src/modules/modal.js
+++ src/modules/modal.js
@@ -4,16 +4,12 @@ export const modal = () => {
      const overlay = document.querySelector('.overlay');
      const services = document.getElementById('services');
      const serviceModal = document.querySelector('.services-modal');
-     const serviceButtons = document.querySelectorAll('.service-button a');
      const modalBlock = document.querySelector('.modals');
      const docs = document.getElementById('documents');
      const bigPhoto = docs.querySelector('.big-photo');
      const sertificates = docs.querySelectorAll('.sertificate-document');
      const documentOverlay = docs.querySelectorAll('.document-overlay');
      
-   /* buttonCall.setAttribute('href', '#');
-     serviceButtons.forEach(serviceButton =>  serviceButton.setAttribute('href', '#'))*/
-
      function changeDisplay(prop, modal) {
        modal.style.display = prop;
        overlay.style.display = prop;
@@ -21,14 +17,12 @@ export const modal = () => {
 
     buttonCall.addEventListener('click', () => {
         changeDisplay('block', headerModal) 
-        return false;
     })
 
     services.addEventListener('click', (e) => {
          if (e.target.closest('.service-button')) {
               changeDisplay('block', serviceModal) 
          }
-         return false;
     })
 
     modalBlock.addEventListener('click', (e) => {
