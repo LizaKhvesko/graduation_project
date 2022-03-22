f17377f9597a626563bf745b64dfd3a0df2298bf
LizaKhvesko <khveskoliza@tut.by>
2022-03-22 13:30:07 +0300

    burger

--- src/modules/burger.js
+++ src/modules/burger.js
@@ -1,7 +1,14 @@
 export const burger = () => {
     const burgerBtn = document.getElementById('navigation');
     const mobileMenu = burgerBtn.querySelector('.navbar-collapse.collapse');
+    let width = document.documentElement.clientWidth;
 
+    if (width > 767 && width <= 992) {
+       burgerBtn.querySelector('.navbar-header').style.display = 'none';
+    } else {
+        burgerBtn.querySelector('.navbar-header').style.display = 'block';
+    }
+   
     burgerBtn.addEventListener('click', (e) => {
         if (e.target.closest('.collapsed')) {
            mobileMenu.classList.toggle('burger-open');
