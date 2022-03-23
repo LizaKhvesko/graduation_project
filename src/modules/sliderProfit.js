4053ad2bd81c45fb0ef99b395b921af3a179178a
LizaKhvesko <khveskoliza@tut.by>
2022-03-23 16:42:25 +0300

    add jpacity in slider benefit

--- src/modules/sliderProfit.js
+++ src/modules/sliderProfit.js
@@ -2,16 +2,19 @@ export const sliderProfit = () => {
     const benefitsItem = Array.from(document.querySelectorAll('.benefits__item'));
     let firstGroup = benefitsItem.slice(0,3);
     let secondGroup = benefitsItem.slice(3);
-    const benefitsArrows = document.querySelector('.benefits-arrows');
+    const benefitsArrows = document.querySelectorAll('.benefits__arrow');
 
-    let width = document.documentElement.clientWidth;
-    
     secondGroup.forEach(item => item.classList.add('noActiveBenefit'));
+    secondGroup.forEach(item => item.classList.add('disappear'));
 
-    benefitsArrows.addEventListener('click', (e) => {
-        if (e.target.closest('.benefits__arrow')) {
-           firstGroup.forEach(item => item.classList.toggle('noActiveBenefit'));
-           secondGroup.forEach(item => item.classList.toggle('noActiveBenefit'));
-        }
+    benefitsArrows.forEach(benefitsArrow => {
+        benefitsArrow.addEventListener('click', (e) => {
+        firstGroup.forEach(item => item.classList.toggle('disappear'))
+        secondGroup.forEach(item => item.classList.toggle('disappear'))
+        setTimeout(() => {
+            secondGroup.forEach(item => item.classList.toggle('noActiveBenefit'));
+            firstGroup.forEach(item => item.classList.toggle('noActiveBenefit'));
+        }, 500)
+    })
     })
 }
